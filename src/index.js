aa1c30e1b6b2c15e3ef07dc8a4c1727403f0e944
Mikhail Zharov <m.zharov@gmail.com>
2023-01-16 08:09:57 +0300

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ var brackets = [];
+
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {      
+      if(str[i] === bracketsConfig[j][1]){
+        if(brackets[brackets.length - 1] == bracketsConfig[j][0]) {
+          brackets.pop();
+          break;
+        } else if (bracketsConfig[j][1] !== bracketsConfig[j][0]) {
+          return false;
+        }
+      }
+      if(str[i] === bracketsConfig[j][0]) {
+        brackets.push(str[i]);
+        break;
+      }
+    }
+  }
+  return brackets.length == 0;
 }
